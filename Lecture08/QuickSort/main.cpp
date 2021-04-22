e2060b2ed87a57e5fc5225ad7f405771e2f645e9
pid011 <pid011@kakao.com>
2021-04-22 16:05:30 +0900

    Add Lecture08 project

---
+++ Lecture08/QuickSort/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <random>
+#include "QuickSort.h"
+
+int main()
+{
+    const int len = 20;
+    QuickSort sort(len);
+
+    std::random_device rand;
+    std::mt19937 randomGen(rand());
+    std::uniform_int_distribution<int> dist(1, len - 1);
+
+    int data[len];
+    for (size_t i = 0; i < len; i++)
+    {
+        data[i] = dist(randomGen);
+    }
+
+    sort.initData(data);
+    sort.draw();
+    sort.sorting();
+    sort.draw();
+}
