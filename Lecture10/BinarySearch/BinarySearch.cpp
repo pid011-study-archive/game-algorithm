f7aae16771c64b4f3ad7be28fc618cedfbd4a574
pid011 <pid011@kakao.com>
2021-05-06 15:55:45 +0900

    Add Lecture10 project

---
+++ Lecture10/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int BinarySearch(int S[], int x, int len)
+{
+    int low = 0;
+    int high = len - 1;
+    int mid;
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (S[mid] == x) return mid;
+
+        if (S[mid] > x)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int S[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    int idx = BinarySearch(S, 3, 9);
+
+    if (idx == -1)
+    {
+        std::cout << "값을 찾지 못함" << std::endl;
+    }
+    else
+    {
+        std::cout << "는 " << idx << "번째에 위치함" << std::endl;
+    }
+}
---
+++ Lecture10/SequentialSearch/SequentialSearch.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+int SearchInt(std::vector<int>& sources, int target)
+{
+    for (int i = 0; i < sources.size(); i++)
+    {
+        if (sources[i] == target) return i;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    std::vector<int> sources { 1, 6, 2, 3, 4, 10 };
+
+    for (auto& i: sources)
+    {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+
+    int idx = SearchInt(sources, 4);
+
+    if (idx == -1)
+        std::cout << "4가 배열에 존재하지 않음" << std::endl;
+    else
+        std::cout << "4는 " << idx << "번째에 위치함" << std::endl;
+}
